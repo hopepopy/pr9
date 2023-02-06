c385ff4f1dcfd0e23d5c31005883be2cef0e6e75
hopepopy <113137385+hopepopy@users.noreply.github.com>
2023-02-06 18:20:51 +0300

    Add files via upload

---
+++ Практ 9/Практ 9/Processes.cs
@@ -0,0 +1,111 @@
+﻿using System.Diagnostics;
+
+namespace Практ_9
+{
+    internal class Processes
+    {
+        public static Process? SelectProcess()
+        {
+            Console.Clear();
+            Console.WriteLine("Список процессов");
+            Console.WriteLine();
+
+            Process[] processes = Process.GetProcesses();
+
+            
+            
+            foreach (Process process in processes)
+            {
+                double memory = Math.Round(process.PrivateMemorySize64 / 1000000d, 2);
+                Console.WriteLine($"  {process.Id}\t| {process.ProcessName}\t\t| {memory}MB");
+            }
+
+            Arrow arrow = new Arrow(2, processes.Length + 1);
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            while (key.Key != ConsoleKey.Escape)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.DownArrow:
+                        arrow.Next();
+                        break;
+                    case ConsoleKey.UpArrow:
+                        arrow.Back();
+                        break;
+                    case ConsoleKey.Enter:
+                        int index = arrow.GetIndex();
+                        return processes[index];
+                }
+                key = Console.ReadKey(true);
+            }
+            return null;
+        }
+        public static void Info(Process process)
+        {
+            Console.Clear();
+            Console.WriteLine($"Информация о процессе {process.ProcessName}");
+            Console.WriteLine();
+        
+            try
+            {
+                TimeSpan time = DateTime.Now - process.StartTime;
+                Console.WriteLine($"Время с запуска: {time}");
+                Console.WriteLine($"Время процессора: {process.TotalProcessorTime}");
+                Console.WriteLine($"Приоритет: {process.BasePriority}");
+                Console.WriteLine($"Класс приоритета: {process.PriorityClass}");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка получения информации. Отказано в доступе.");
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.WriteLine("D - завершить процесс");
+                Console.WriteLine("Del - завершить все процессы с этим именем");
+            }
+
+
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            while (key.Key != ConsoleKey.Backspace)
+            {
+                switch (key.Key)
+                {
+                    case ConsoleKey.D:
+                        try
+                        {
+                            process.Kill();
+                            return;
+                        } 
+                        catch
+                        {
+                            int line = Console.CursorTop;
+                            Console.SetCursorPosition(0, line + 1);
+                            Console.WriteLine("D   Отказано в доступе.");
+                            Console.SetCursorPosition(0, line);
+                        }
+                        break;
+                    case ConsoleKey.Delete:
+                        Process[] processes = Process.GetProcessesByName(process.ProcessName);
+                        try
+                        {
+                            foreach (Process proc in processes)
+                            {
+                                proc.Kill();
+                            }
+                            return;
+                        }
+                        catch
+                        {
+                            int line = Console.CursorTop;
+                            Console.SetCursorPosition(0, line + 1);
+                            Console.WriteLine("Del Отказано в доступе.");
+                            Console.SetCursorPosition(0, line);
+                        }
+                        break;
+                }
+                key = Console.ReadKey(true);
+            }
+        }
+    }
+}
---
+++ Практ 9/Практ 9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+
+namespace Практ_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Process? process = Processes.SelectProcess();
+                if (process == null)
+                {
+                    break;
+                }
+                Processes.Info(process);
+            }
+        }
+    }
+}
